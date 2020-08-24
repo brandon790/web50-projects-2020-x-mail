7ee979a226c3b06b9de511f555e36454dbc3b869
brandon790 <67201798+brandon790@users.noreply.github.com>
2020-08-23 21:34:40 -0400

    show emails partial working

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,6 +15,9 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#emails').style.display = 'none';
+  
+  
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -22,12 +25,63 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+  //post email
+document.addEventListener('submit', function() {
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: document.querySelector("#compose-recipients").value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  })
+});
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+ //get and show emails
+ document.querySelector('#emails-view')
+ const emails = []
+ fetch(`/emails/${mailbox}`)
+ .then(response => response.json())
+ .then(emails => {
+     // Print emails
+     console.log(emails);
+
+     //array of each field
+    let sender = emails.map(a => a.sender);
+    let subject = emails.map(a => a.subject);
+    let timestamp = emails.map(a => a.timestamp);
+
+    //display emails
+    var i;
+    for (i = 0; i < emails.length; i++) {
+    var newDiv = document.createElement("div"); 
+    newDiv.className = "email-class";
+    newDiv.style.border = "thin solid";
+    newDiv.style.borderColor = "black";
+    newDiv.style.padding = "15px";
+    newDiv.style.marginLeft = "210px";
+    newDiv.style.marginRight = "210px";
+    let a = sender[i];
+    let b = subject[i];
+    let c = timestamp[i];
+    newDiv.innerHTML = `${a}  ${b}  ${c}`;
+    document.body.appendChild(newDiv);
+    }
+    });
+
 }
